0458ba5473dd43df2ee6869290a30cb29b864176
Luke A <legojedidev@users.noreply.github.com>
2020-06-29 21:55:37 -0500

    Made a new project

---
+++ Luke Practice/Go home/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float moveSpeed;
+
+    private Vector3 input;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        input = new Vector3(input.getAxis("Horizontal"), 0, input.getAxis("Vertical"));
+        GetComponent<Rigidbody>("Player").addForce(input * moveSpeed);
+    }
+}
